fd759b8a8d578c44df5698626053ebc3cc20438f
SpeedProg <speedprogde@googlemail.com>
2016-02-25 23:14:21 +0100

    add ability to link characters on fc creation script

--- create_fc.py
+++ create_fc.py
@@ -1,7 +1,7 @@
-from waitlist.storage.database import Account
+from waitlist.storage.database import Account, Character, session, Role
 from waitlist.utils import get_random_token
-from waitlist import storage
 from waitlist.permissions import WTMRoles
+import evelink
 if __name__ == '__main__':
     name = raw_input("Login Name:")
     pw = raw_input("Password:")
@@ -13,7 +13,26 @@ if __name__ == '__main__':
     acc.login_token = get_random_token(64)
     acc.email = email
     print "Account created"
-    acc.roles.append(WTMRoles.fc)
-    storage.database.session.add(acc)
-    storage.database.session.commit()
+    fc_role = session.query(Role).filter(Role.name == WTMRoles.fc.name).first()
+    acc.roles.append(fc_role)
+    session.add(acc)
     print acc.login_token
+    
+    char_name = "--"
+    eve = evelink.eve.EVE()
+    while char_name:
+        char_name = raw_input("Enter Character to associate with this account:")
+        char_name = char_name.strip()
+        if not char_name:
+            break
+        
+        response = eve.character_id_from_name(char_name)
+        char_id = int(response.result)
+        character = Character()
+        character.eve_name = char_name
+        character.id = char_id
+        acc.characters.append(character)
+    
+    session.commit()
+    
+            
