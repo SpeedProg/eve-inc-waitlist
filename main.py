48c06b6692b972af9527c449dde1d185d05bac9a
SpeedProg <speedprogde@googlemail.com>
2016-02-27 17:45:58 +0100

    added user id to waitlist entry and some comments

--- main.py
+++ main.py
@@ -206,21 +206,24 @@ def xup_submit():
     if len(logi) and logi_entry == None:
         logi_entry = WaitlistEntry()
         logi_entry.user = get_char_id()
-        logi_entry.creation = creationdt
+        logi_entry.creation = creationdt # for sorting entries
+        logi_entry.user = current_user.get_eve_id() # associate a user with the entry
         add_entries_map[WaitlistNames.logi] = logi_entry
     
     # same for dps
     if len(dps) and dps_entry == None:
         dps_entry = WaitlistEntry()
         dps_entry.user = get_char_id()
-        dps_entry.creation = creationdt
+        dps_entry.creation = creationdt # for sorting entries
+        dps_entry.user = current_user.get_eve_id() # associate a user with the entry
         add_entries_map[WaitlistNames.dps] = dps_entry
 
     # and sniper
     if len(sniper) and sniper_entry == None:
         sniper_entry = WaitlistEntry()
         sniper_entry.user = get_char_id()
-        sniper_entry.creation = creationdt
+        sniper_entry.creation = creationdt # for sorting entries
+        sniper_entry.user = current_user.get_eve_id() # associate a user with the entry
         add_entries_map[WaitlistNames.sniper] = sniper_entry
 
     # iterate over sorted fits and add them to their entry
