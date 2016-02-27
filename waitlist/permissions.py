71eaa6689b88f7beda560a107c4c83224727ae62
SpeedProg <speedprogde@googlemail.com>
2016-02-27 17:40:52 +0100

    changed the way default roles are saved

--- waitlist/permissions.py
+++ waitlist/permissions.py
@@ -1,11 +1,8 @@
-from waitlist.storage.database import Role
-from waitlist.setup_wtm import get_role
-
 class WTMRoles():
-    admin = get_role("admin")
-    officer = get_role("officer")
-    fc = get_role("fc")
-    lm = get_role("lm")
-    tbag = get_role("tbag")
-    resident = get_role("resident")
+    admin = "admin"
+    officer = "officer"
+    fc = "fc"
+    lm = "lm"
+    tbag = "tbag"
+    resident = "resident"
     
