6253cebb8e82924f50e7de3bc030a97144200c14
SpeedProg <speedprogde@googlemail.com>
2016-05-11 14:59:37 +0200

    fixed static file rename script

--- rename_static_files.py
+++ rename_static_files.py
@@ -5,11 +5,13 @@ import os
 if __name__ == '__main__':
     v = version
     basePath = "./static/js"
-    onlyfiles = [f for f in listdir(basePath) if isfile(join(basePath, f))]
+    onlyfiles = [join(basePath, f) for f in listdir(basePath) if isfile(join(basePath, f))]
     for f in onlyfiles:
+        print f
         os.rename(f, f.replace(".js", ""+v+".js"))
     
         basePath = "./static/css"
-    onlyfiles = [f for f in listdir(basePath) if isfile(join(basePath, f))]
+    onlyfiles = [join(basePath, f) for f in listdir(basePath) if isfile(join(basePath, f))]
     for f in onlyfiles:
+        print f
         os.rename(f, f.replace(".css", ""+v+".css"))
