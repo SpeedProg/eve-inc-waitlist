1ec0fdddb555e37d8c04e90149fbd639d018b584
SpeedProg <speedprogde@googlemail.com>
2016-08-03 17:52:44 +0200

    number of unique voters per lm/fc

--- waitlist/blueprints/options/ccvote_results.py
+++ waitlist/blueprints/options/ccvote_results.py
@@ -3,7 +3,7 @@ import logging
 from flask_login import login_required
 from waitlist.data.perm import perm_leadership
 from flask.templating import render_template
-from waitlist.storage.database import CCVote, Account
+from waitlist.storage.database import CCVote, Account, Character
 from waitlist.base import db
 from sqlalchemy.sql.functions import func
 bp = Blueprint('settings_ccvote', __name__)
@@ -27,4 +27,23 @@ def index():
         ).order_by(
                    func.count('*')
         ).all()
-    return render_template("settings/ccvotes.html", fcs=fcResults, lms=lmResults)
+    
+    # unique votes
+    unique_votes_query_fc = db.session.query(CCVote.voterID.label("voterID"), CCVote.fcvoteID.label("fcvoteID")).distinct().subquery()
+    unique_votes_query_lm = db.session.query(CCVote.voterID.label("voterID"), CCVote.lmvoteID.label("lmvoteID")).distinct().subquery()
+    
+    unqiueFCVotes = db.session.query(Account.username, func.count('*')
+        ).join(unique_votes_query_fc, unique_votes_query_fc.c.fcvoteID==Account.id
+        ).group_by(
+                   Account.username
+        ).order_by(
+                   func.count('*')
+        ).all()
+    unqiueLMVotes = db.session.query(Account.username, func.count('*')
+        ).join(unique_votes_query_lm, unique_votes_query_lm.c.lmvoteID==Account.id
+        ).group_by(
+                   Account.username
+        ).order_by(
+                   func.count('*')
+        ).all()
+    return render_template("settings/ccvotes.html", fcs=fcResults, lms=lmResults, ufcs=unqiueFCVotes, ulms=unqiueLMVotes)
