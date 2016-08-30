da341029a173c6d7adc0a1d6ed699f1a5ab016d3
SpeedProg <speedprogde@googlemail.com>
2016-08-30 19:24:26 +0200

    fixed an exception caused by a server error response

--- waitlist/utility/fleet.py
+++ waitlist/utility/fleet.py
@@ -282,9 +282,11 @@ def invite(user_id, squadIDList):
         else:
             try:
                 logger.error("CREST failed with %s : %s", str(ex.resp.status_code), ex.resp.json())
+                return {'status_code': ex.resp.status_code, 'text': ex.resp.json()['error_description']}
             except ValueError:
                 logger.error("CREST failed with %s : %s", str(ex.resp.status_code), ex.resp.text)
-            return {'status_code': ex.resp.status_code, 'text': ex.resp.json()['error_description']}
+                return {'status_code': ex.resp.status_code, 'text': ex.resp.text}
+            
     logger.info("Failed to invite %d to a squad, because all squads are full!", user_id)
     return {'status_code': 403, 'text': 'Failed to invite person a a squad, all squads are full!'}
 
