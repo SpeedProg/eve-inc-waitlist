addbadc46e007bd56de26b4c4638d7b316a7252f
SpeedProg <speedprogde@googlemail.com>
2016-09-21 19:10:30 +0200

    added some ticket stuff in preperation for opening mails

---
+++ static/js/ticket-settings.js
@@ -0,0 +1,30 @@
+
+function getTicketElement(ticketId) {
+	var el = {
+			id: ticketId,
+			jqE: $('#fb-'+ticketId)
+	}
+	el.getTitle = function(){
+		return $(":nth-child(4)", this.jqE).text();
+	}
+	
+	el.getMessage = function() {
+		return $(":nth-child(5)", this.jqE).text();
+	}
+	
+	el.getCharacterId = function() {
+		return this.jqE.attr('data-charID');
+	}
+	el.getCharacterName = function() {
+		return $(":nth-child(3)", this.jqE).text();
+	}
+	return el;
+}
+
+function sendTicketMail(ticketId) {
+	var ticketElement = getTicketElement(ticketId);
+	var title = ticketElement.getTitle();
+	var message = ticketElement.getMessage();
+	var charID = ticketElement.getCharacterId();
+	var charName = ticketElement.getCharacterName();
+}
