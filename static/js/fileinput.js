ea382563a1271e373446f89baf496680a87528e7
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-17 20:12:38 +0100

    Update fileinput so that file name is visable when selecting a file.

---
+++ static/js/fileinput.js
@@ -0,0 +1,33 @@
+function nameFiles() {
+	$('body').on('change', 'input[type="file"][data-toggle="custom-file"]', function (ev) {
+
+	  const $input = $(this);
+	  const target = $input.data('target');
+	  const $target = $(target);
+
+	  if (!$target.length)
+		return console.error('Invalid target for custom file', $input);
+
+	  if (!$target.attr('data-content'))
+		return console.error('Invalid `data-content` for custom file target', $input);
+
+	  // set original content so we can revert if user deselects file
+	  if (!$target.attr('data-original-content'))
+		$target.attr('data-original-content', $target.attr('data-content'));
+
+	  const input = $input.get(0);
+
+	  let name = $.type(input)
+		&& $.type(input.files) !== "undefined"
+		&& $.type(input.files[0]) !== "undefined"
+		&& $.type(input.files[0].name) === "string" ? input.files[0].name : $input.val();
+
+	  if ($.type(name) === "null" || name === '')
+		name = $target.attr('data-original-content');
+
+	  $target.attr('data-content', name);
+
+	});
+}
+
+document.addEventListener('DOMContentLoaded', nameFiles);
