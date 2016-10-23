b8a41a1d199e9ac8f3028322bf2c9ded761b0276
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-23 03:25:30 +0100

    Remember cookie http only enabled

--- waitlist/base.py
+++ waitlist/base.py
@@ -30,6 +30,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
+app.config['REMEMBER_COOKIE_HTTPONLY'] = True
 seasurf = SeaSurf(app)
 app.config['UPLOAD_FOLDER'] = path.join(".", "sde")
 app.config['MINIFY_PAGE'] = config.html_min
