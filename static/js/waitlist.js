5a97c586bae5306c67a13e0843c3d856aa1fae2b
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-25 16:04:05 +0100

    Update to fix status dropdown icon

--- static/js/waitlist.js
+++ static/js/waitlist.js
@@ -75,15 +75,22 @@ waitlist.linemember = (function() {
 		removeOwnEntry(wlId, charId, entryId);
 	}
 
+	function minusPlus(event) {
+		$(event).removeClass("fa-minus-square").addClass("fa-plus-square");
+	}
+
+	function plusMinus(event) {
+		$(event).removeClass("fa-plus-square").addClass("fa-minus-square");
+	}
+
 	function waitlistCollapseHandler(event) {
 		event.stopPropagation();
 		var target = $(event.currentTarget);
 		var id = target.attr("id");
-		var togglerSelector = target.attr("data-tog-icon");
+		var togglerSelector = target.data("tog-icon");
 		if (togglerSelector !== undefined || togglerSelector !== null) {
 			localStorage.setItem(id, 'closed');
-			$(togglerSelector).removeClass("fa-minus-square").addClass(
-				"fa-plus-square");
+			minusPlus(togglerSelector);
 		}
 	}
 
@@ -94,8 +101,23 @@ waitlist.linemember = (function() {
 		var togglerSelector = target.data("tog-icon");
 		if (togglerSelector !== undefined || togglerSelector !== null) {
 			localStorage.removeItem(id);
-			$(togglerSelector).removeClass("fa-plus-square").addClass(
-				"fa-minus-square");
+			plusMinus(togglerSelector);
+		}
+	}
+
+	function statusCollapseHandler(event) {
+		event.stopPropagation();
+		var togglerSelector = $(event.currentTarget).data("tog-icon");
+		if (togglerSelector !== undefined || togglerSelector !== null) {
+			minusPlus(togglerSelector);
+		}
+	}
+
+	function statusExpandHandler(event) {
+		event.stopPropagation();
+		var togglerSelector = $(event.currentTarget).data("tog-icon");
+		if (togglerSelector !== undefined || togglerSelector !== null) {
+			plusMinus(togglerSelector);
 		}
 	}
 
@@ -122,6 +144,12 @@ waitlist.linemember = (function() {
 			waitlistExpandeHandler);
 		$('#waitlists').on('hide.bs.collapse', '.collapse',
 			waitlistCollapseHandler);
+
+		// setup status close/open event handlers
+		$('#status').on('show.bs.collapse', '.collapse',
+			statusExpandHandler);
+		$('#status').on('hide.bs.collapse', '.collapse',
+			statusCollapseHandler);
 
 		// load old states of the waitlists
 		var wlists = $('ol[id|="wl-fits"]');
