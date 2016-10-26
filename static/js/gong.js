af0d807dcd12b0ebc1a448780385ff20a1ded979
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-26 23:37:23 +0100

    Gong Cleanup & gongDisable function setup and exported

--- static/js/gong.js
+++ static/js/gong.js
@@ -6,9 +6,11 @@ if (!waitlist) {
 
 waitlist.gong = (function() {
 
+	const displayMessage = waitlist.base.displayMessage;
 	const addListener = waitlist.sse.addEventListener;
-	const gongbutton = document.getElementById("gongbutton");
-	const sound = document.getElementById("sound");
+	const storage = sessionStorage;
+	var gongbutton;
+	var sound;
 
 	function playGong() {
 		if (gongbutton.checked) {
@@ -21,32 +23,47 @@ waitlist.gong = (function() {
 		if (gongbutton.checked) {
 			sound.removeAttribute("hidden");
 			sound.volume = 0.5;
-			sessionStorage.setItem('gong', 'open');
+			storage.setItem("gong", "open");
 		} else {
 			sound.setAttribute("hidden", "");
 			sound.pause();
-			sessionStorage.removeItem('gong');
+			storage.removeItem("gong");
 		}
+	}
+
+	function gongDisable() {
+		gongbutton.checked = false;
+		sound.setAttribute("hidden", "");
+		sound.pause();
 	}
 
 	function gongSetup() {
-		// Check if browser supports event source
-		if (!!window.EventSource && gongbutton) {
-			// Add click handler & add eventlistener & check Session Storage
-			addListener("invite-send", playGong);
-			gongbutton.addEventListener("click", gongClicked);
-			if (sessionStorage.getItem('gong')) {
-				gongbutton.checked = true;
-				gongClicked();
-			}
+		// Setup SSE invite-send event
+		addListener("invite-send", playGong);
+		gongbutton.addEventListener("click", gongClicked);
+		// Checks storage for gong info if not found alert to please enable notification
+		if (storage.getItem("gong")) {
+			gongbutton.checked = true;
+			gongClicked();
 		} else {
-			// If not remove button
-			if (gongbutton) {
-				gongbutton.parentNode.parentNode.remove();
+			displayMessage("To get informed when you are invited please enable browser notifications in the top right.", "info");
+		}
+	}
+
+	function init() {
+		gongbutton = document.getElementById(gongbutton);
+		sound = document.getElementById(sound);
+		if (gongbutton) {
+			if (!!window.EventSource) {
+				gongSetup();
+			} else {
+				document.getElementById(gong).remove();
 			}
 		}
 	}
 
-	$(document).ready(gongSetup);
-	return {};
+	$(document).ready(init);
+	return {
+	gongDisable: gongDisable
+	};
 })();
