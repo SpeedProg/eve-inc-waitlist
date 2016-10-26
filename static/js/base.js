65dd6526f7d13438fb05f713f871dd28f1c16876
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-26 23:18:47 +0100

    Update displayMessage to allow html tags if set in arguments

--- static/js/base.js
+++ static/js/base.js
@@ -9,7 +9,7 @@ waitlist.base = (function(){
 		return $('meta[name="'+name+'"]').attr('content');
 	}
 	
-	function displayMessage(message, type) {
+	function displayMessage(message, type, html=false) {
 		var alertHTML = $($.parseHTML(`<div class="alert alert-dismissible alert-${type}" role="alert">
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				<span aria-hidden="true">&times;</span>
@@ -17,7 +17,11 @@ waitlist.base = (function(){
 				<p class="text-xs-center"></p>
 				</div>`));
 		var textContainer = $('.text-xs-center', alertHTML);
-		textContainer.text(message);
+		if (html === true) {
+			textContainer.html(message);
+		} else {
+			textContainer.text(message);
+		}
 		var alertArea = $('#alert-area-base');
 		alertArea.append(alertHTML);
 	}
