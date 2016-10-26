be58e91f9e45bb187595dd448395939ee176b804
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-26 21:29:00 +0100

    Simplify dropdowns and dropdown state loading

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -5,67 +5,43 @@ if (!waitlist) {
 }
 
 waitlist.dropdown = (function() {
-	function minusPlus(event) {
-		$(event).removeClass("fa-minus-square").addClass("fa-plus-square");
-	}
 
-	function plusMinus(event) {
-		$(event).removeClass("fa-plus-square").addClass("fa-minus-square");
-	}
-
-	function waitlistCollapseHandler(event) {
+	function dropdownHandler(event) {
 		event.stopPropagation();
-		var target = $(event.currentTarget);
-		var id = target.attr("id");
-		var togglerSelector = target.data("tog-icon");
-		if (togglerSelector !== undefined || togglerSelector !== null) {
-			localStorage.setItem(id, 'closed');
-			minusPlus(togglerSelector);
+		const icon = event.target.dataset.togIcon;
+		if (event.type === "show") {
+			$(icon).addClass("fa-minus-square").removeClass("fa-plus-square");
+		}
+		if (event.type === "hide") {
+			$(icon).addClass("fa-plus-square").removeClass("fa-minus-square");
+		}
+		if (event.target.id.startsWith("wl-fits-")) {
+			manageDropdownState(event);
 		}
 	}
 
-	function waitlistExpandeHandler(event) {
-		event.stopPropagation();
-		var target = $(event.currentTarget);
-		var id = target.attr("id");
-		var togglerSelector = target.data("tog-icon");
-		if (togglerSelector !== undefined || togglerSelector !== null) {
-			localStorage.removeItem(id);
-			plusMinus(togglerSelector);
+	function manageDropdownState(event) {
+		if (event.type === "show") {
+			localStorage.removeItem(event.target.id);
 		}
-	}
-
-	function statusCollapseHandler(event) {
-		event.stopPropagation();
-		var togglerSelector = $(event.currentTarget).data("tog-icon");
-		if (togglerSelector !== undefined || togglerSelector !== null) {
-			minusPlus(togglerSelector);
-		}
-	}
-
-	function statusExpandHandler(event) {
-		event.stopPropagation();
-		var togglerSelector = $(event.currentTarget).data("tog-icon");
-		if (togglerSelector !== undefined || togglerSelector !== null) {
-			plusMinus(togglerSelector);
+		if (event.type === "hide") {
+			localStorage.setItem(event.target.id, "closed");
 		}
 	}
 
 	function init() {
-		// setup waitlist close/opne event handlers
-		$('#waitlists').on('show.bs.collapse', '.collapse',
-			waitlistExpandeHandler);
-		$('#waitlists').on('hide.bs.collapse', '.collapse',
-			waitlistCollapseHandler);
+		// Setup open & close event handler
+		$("#content").on("hide.bs.collapse show.bs.collapse", ".collapse", dropdownHandler);
 
-		// setup status close/open event handlers
-		$('#status').on('show.bs.collapse', '.collapse',
-			statusExpandHandler);
-		$('#status').on('hide.bs.collapse', '.collapse',
-			statusCollapseHandler);
+        // Load previous state of the waitlist
+        const wlists = Array.from($("ol[id|='wl-fits']"));
+        wlists.forEach( function(wlist) {
+            if (localStorage.getItem(wlist.id) !== null) {
+            	$(wlist).collapse("hide");
+            }
+		});
 	}
 
 	$(document).ready(init);
-
 	return {};
 })();
