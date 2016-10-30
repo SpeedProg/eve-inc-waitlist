9f65c3371d53aef1d8d0264eae35809d5f72bb49
Beryl Slanjava <berylslanjava@icloud.com>
2016-10-30 08:29:42 +0000

    Remove SSE requirement for time updating.

--- static/js/waitlist-dom.js
+++ static/js/waitlist-dom.js
@@ -719,9 +719,7 @@ waitlist.listdom = (function(){
 		settings.can_view_fits = getMetaData('can-view-fits') === "True";
 		settings.can_manage = getMetaData('can-fleetcomp') === "True";
 		settings.user_id = Number(getMetaData('user-id'));
-		if (!!window.EventSource) {
-			setInterval(updateWaitTimes, 30000);
-		}
+		setInterval(updateWaitTimes, 30000);
 	}
 
 	
