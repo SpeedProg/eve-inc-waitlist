dcf8504da5ebb752fedb5df163964e5bde990296
SpeedProg <speedprogde@googlemail.com>
2016-10-30 15:43:59 +0100

    add rate limiting to the public waitlists api endpoint max request 5 different groups per minute and every specific group only once per minute same with waitlists

--- waitlist/blueprints/api/waitlist.py
+++ waitlist/blueprints/api/waitlist.py
@@ -2,33 +2,62 @@ from __future__ import absolute_import
 from flask.views import MethodView
 from flask.blueprints import Blueprint
 import logging
-from waitlist.base import db
+from waitlist.base import db, limiter
 from waitlist.storage.database import WaitlistGroup, Waitlist
 from flask import jsonify
 from waitlist.utility.json import makeJsonGroups, makeJsonGroup, makeJsonWaitlistsBaseData,\
  makeJsonWaitlistBaseData
 from click.decorators import group
 import flask
+from flask_login import current_user
+from flask.globals import request
+from flask_limiter.util import get_ipaddr
+from urllib import urlencode
 
 bp = Blueprint('api_waitlists', __name__)
 logger = logging.getLogger(__name__)
 
+def get_ratekey_func_with_arguments(arglist):
+    def ratekeyfunc():
+        addr = get_ipaddr();
+        params = {}
+        for argname in arglist:
+            params[argname] = str(request.view_args[argname])
+            
+        # we need to encode the url properly instead of just doing &name=value...
+        # so some one can't insert stuff to make the request unique
+        key = addr + urlencode(params)
+        return key
+    return ratekeyfunc
+
 class WaitlistGroupsAPI(MethodView):
+    decorators = [
+        limiter.limit("1/minute", key_func=get_ratekey_func_with_arguments(['groupID']), exempt_when=lambda: current_user.is_authenticated),
+        limiter.limit("5/minute", exempt_when=lambda: current_user.is_authenticated)
+    ]
     def get(self, groupID):
         if groupID is None:
             groups = db.session.query(WaitlistGroup).all();
             return jsonify(makeJsonGroups(groups))
         else:
             group = db.session.query(WaitlistGroup).get(groupID)
+            if group is None:
+                flask.abort(404, "No such group")
             return jsonify(makeJsonGroup(group))
 
 class WaitlistBaseDataAPI(MethodView):
+    decorators = [
+        limiter.limit("1/minute", key_func=get_ratekey_func_with_arguments(['waitlistID']), exempt_when=lambda: current_user.is_authenticated),
+        limiter.limit("5/minute", exempt_when=lambda: current_user.is_authenticated)
+    ]
     def get(self, waitlistID):
         if waitlistID is None:
             waitlists = db.session.query(Waitlist).all();
             return jsonify(makeJsonWaitlistsBaseData(waitlists))
         else:
             waitlist = db.session.query(Waitlist).get(waitlistID)
+            if waitlist is None:
+                flask.abort(404, "No such waitlist")
             if waitlistID is None:
                 flask.abort(404, 'No Waitlist with this ID found')
             return jsonify(makeJsonWaitlistBaseData(waitlist))
