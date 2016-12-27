a801e3b2dde693ad96897c14f12a8c95f22b3329
SpeedProg <speedprogde@googlemail.com>
2016-12-27 14:53:02 +0100

    implemented the role change signal handler that input the the information into the database

--- waitlist/signal/signals.py
+++ waitlist/signal/signals.py
@@ -1,5 +1,8 @@
-from blinker.base import Signal
+from blinker.base import Namespace
 SIG_ROLES_EDITED = "roles-edited"
-roles_changed_sig = Signal(SIG_ROLES_EDITED)
-def sendRolesChanged(toID, byID, added_roles, removed_roles, note):
-    pass
+
+waitlist_bps = Namespace();
+
+roles_changed_sig = waitlist_bps.signal(SIG_ROLES_EDITED, "Called when roles are changed on an account")
+def sendRolesChanged(sender, toID, byID, added_roles, removed_roles, note):
+    roles_changed_sig.send(sender, toID=toID, byID=byID, added_roles=added_roles, removed_roles=removed_roles, note=note)
