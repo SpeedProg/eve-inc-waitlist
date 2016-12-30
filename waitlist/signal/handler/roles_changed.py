dc0f4626690f384a4c8529efd21515e6679ba69d
SpeedProg <speedprogde@googlemail.com>
2016-12-30 21:23:34 +0100

    finialized adding notes to people (possibly connected directly to role changes)

--- waitlist/signal/handler/roles_changed.py
+++ waitlist/signal/handler/roles_changed.py
@@ -2,7 +2,12 @@ from waitlist.storage.database import RoleHistoryEntry, RoleChangeEntry#
 from waitlist.base import db
 from waitlist.storage.database import Role
 from sqlalchemy.sql.expression import or_
+from waitlist.signal.signals import roles_changed_sig
+
+@roles_changed_sig.connect
 def onRolesChanged(sender, toID, byID, added_roles, removed_roles, note):
+    if (len(added_roles) <= 0 and len(removed_roles) <= 0 and note == ""):
+        return
     historyEntry = RoleHistoryEntry(accountID=toID, byAccountID=byID, note=note)
     if len(added_roles) > 0:
         db_roles = db.session.query(Role).filter(or_(Role.name == name for name in added_roles)).all()
@@ -16,3 +21,5 @@ def onRolesChanged(sender, toID, byID, added_roles, removed_roles, note):
         for role in db_roles:
             role_change = RoleChangeEntry(added=False, role=role)
             historyEntry.role_changes.append(role_change)
+    db.session.add(historyEntry)
+    db.session.commit()
