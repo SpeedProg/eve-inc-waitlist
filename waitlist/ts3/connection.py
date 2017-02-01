5ff4c6690e76318091ee43e676a25e0250b9f06f
SpeedProg <speedprogde@googlemail.com>
2017-02-01 15:38:28 +0100

    added config option to disable teamspeak

--- waitlist/ts3/connection.py
+++ waitlist/ts3/connection.py
@@ -1,5 +1,7 @@
 from ts3.query import TS3Connection, TS3QueryError
 import logging
+
+from waitlist.utility import config
 from waitlist.utility.settings.settings import sget_active_ts_id
 from waitlist.storage.database import TeamspeakDatum
 from waitlist.base import db
@@ -8,6 +10,9 @@ from time import sleep
 logger = logging.getLogger(__name__)
 
 def make_connection():
+    if config.disable_teamspeak:
+        return None
+
     teamspeakID = sget_active_ts_id()
     if teamspeakID is None:
         return None
@@ -37,12 +42,16 @@ def make_connection():
 conn = make_connection()
 
 def change_connection():
+    if config.disable_teamspeak:
+        return
     global conn
     if conn is not None:
         conn.quit()
     conn = make_connection()
 
 def handle_dc(func, *args, **kwargs):
+    if config.disable_teamspeak:
+        return
     def func_wrapper(*args, **kwargs):
         global conn
         if conn is not None:
@@ -68,6 +77,8 @@ def handle_dc(func, *args, **kwargs):
 
 @handle_dc
 def send_poke(name, msg):
+    if config.disable_teamspeak:
+        return
     global conn
     try:
         response = conn.clientfind(pattern=name)
@@ -92,6 +103,8 @@ def send_poke(name, msg):
 
 @handle_dc
 def move_to_safety_channel(name, channelID):
+    if config.disable_teamspeak:
+        return
     try:
         response = conn.clientfind(pattern=name)
     except TS3QueryError as er:
@@ -114,4 +127,4 @@ def move_to_safety_channel(name, channelID):
     if client is None: # we didn't find a user
         return
     conn.clientmove(resp['clid'], channelID)
-    return;
+    return
