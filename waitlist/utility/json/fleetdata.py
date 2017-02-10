967c0098c3472fd84016edf77c31e5c1915e9e47
SpeedProg <speedprogde@googlemail.com>
2017-02-10 01:03:14 +0100

    fixed fleetmember json formatter

--- waitlist/utility/json/fleetdata.py
+++ waitlist/utility/json/fleetdata.py
@@ -1,5 +1,7 @@
 
 import json
+from datetime import datetime
+
 from waitlist.utility.swagger.eve.fleet.models import FleetMember
 
 
@@ -7,5 +9,7 @@ class FleetMemberEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, FleetMember):
             return obj._data
+        if isinstance(obj, datetime):
+            return obj.isoformat()
         # Let the base class default method raise the TypeError
         return json.JSONEncoder.default(self, obj)
