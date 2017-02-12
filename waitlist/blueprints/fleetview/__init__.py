656af4e1c5c7d942fb7501a72d98e2c585269e1f
SpeedProg <speedprogde@googlemail.com>
2017-02-12 23:10:50 +0100

    cleaned up main.py so it doesn't contain other things then the startup code anymore

---
+++ waitlist/blueprints/fleetview/__init__.py
@@ -0,0 +1,19 @@
+import logging
+from flask import Blueprint
+from flask import render_template
+from flask_login import login_required
+
+from waitlist.base import db
+from waitlist.permissions import perm_manager
+from waitlist.storage.database import CrestFleet
+
+bp = Blueprint('fleetview', __name__)
+logger = logging.getLogger(__name__)
+
+
+@bp.route("/")
+@login_required
+@perm_manager.require('dev')
+def index():
+    fleets = db.session.query(CrestFleet).filter((CrestFleet.comp != None) & (CrestFleet.group != None)).all()
+    return render_template("settings/fleetspy/spy.html", fleets=fleets)
---
+++ waitlist/utility/login/__init__.py
@@ -0,0 +1,49 @@
+import flask
+import logging
+from flask import current_app
+from flask import redirect
+from flask import url_for
+from flask.ext.login import login_user
+from flask.ext.principal import identity_changed, Identity
+
+from waitlist.base import db
+from waitlist.blueprints.fc_sso import add_sso_handler
+from waitlist.sso import authorize, whoAmI
+from waitlist.storage.database import Account
+from waitlist.utility.eve_id_utils import get_character_by_id_and_name, is_char_banned
+
+logger = logging.getLogger(__name__)
+
+
+def member_login_cb(code):
+    auth = authorize(code)
+    access_token = auth['access_token']
+
+    authInfo = whoAmI(access_token)
+    charID = authInfo['CharacterID']
+    charName = authInfo['CharacterName']
+
+    if charID is None or charName is None:
+        flask.abort(400, "Getting Character from AuthInformation Failed!")
+
+    char = get_character_by_id_and_name(charID, charName)
+
+    # see if there is an fc account connected
+    acc = db.session.query(Account).filter(
+        (Account.username == char.get_eve_name()) & (Account.disabled is False)).first()
+    if acc is not None:  # accs are allowed to ignore bans
+        login_user(acc, remember=True)
+        identity_changed.send(current_app._get_current_object(),
+                              identity=Identity(acc.id))
+        return redirect(url_for("index"))
+
+    is_banned, reason = is_char_banned(char)
+    if is_banned:
+        return flask.abort(401, 'You are banned, because your ' + reason + " is banned!")
+
+    login_user(char, remember=True)
+    logger.debug("Member Login by %s successful", char.get_eve_name())
+    return redirect(url_for("index"))
+
+
+add_sso_handler('linelogin', member_login_cb)
