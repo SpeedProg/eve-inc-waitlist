69f2eb1155e1f6847787cb9ec4970de5c0ec5e61
SpeedProg <speedprogde@googlemail.com>
2017-02-12 23:10:50 +0100

    cleaned up main.py so it doesn't contain other things then the startup code anymore

---
+++ waitlist/utility/flask/__init__.py
@@ -0,0 +1,91 @@
+import logging
+from datetime import datetime
+
+import flask
+import math
+from flask import current_app
+from flask import render_template
+from flask_login import current_user, login_manager, login_required, logout_user
+from flask_principal import Identity, identity_changed, AnonymousIdentity, UserNeed, RoleNeed, identity_loaded
+from sqlalchemy.exc import StatementError
+
+from waitlist.base import principals, app, db
+from waitlist.blueprints.fc_sso import get_sso_redirect
+from waitlist.storage.database import Account
+from waitlist.utility.account import force_logout
+from waitlist.utility.eve_id_utils import is_char_banned, get_account_from_db, get_char_from_db
+
+logger = logging.getLogger(__name__)
+
+
+@principals.identity_loader
+def load_identity_when_session_expires():
+    if hasattr(current_user, 'get_id'):
+        return Identity(current_user.get_id())
+
+
+@app.before_request
+def check_ban():
+    if current_user.is_authenticated:
+        if current_user.type == "character":
+            is_banned, _ = is_char_banned(current_user)
+            if is_banned:
+                force_logout()
+        elif current_user.type == "account":
+            if current_user.disabled:
+                force_logout()
+
+
+@login_manager.user_loader
+def load_user(unicode_id):
+    # it is an account
+    try:
+        return get_user_from_db(unicode_id)
+    except StatementError:
+        db.session.rollback()
+        logger.exception("Failed to get user from db")
+        return get_user_from_db(unicode_id)
+
+
+def get_user_from_db(unicode_id):
+    if unicode_id.startswith("acc"):
+        unicode_id = unicode_id.replace("acc", "", 1)
+        return get_account_from_db(int(unicode_id))
+
+    if unicode_id.startswith("char"):
+        unicode_id = unicode_id.replace("char", "", 1)
+        return get_char_from_db(int(unicode_id))
+
+    return None
+
+
+@identity_loaded.connect_via(app)
+def on_identity_loaded(sender, identity):
+    # Set the identity user object
+    identity.user = current_user
+    # Add the UserNeed to the identity
+    logger.info("loading identity for %s", current_user)
+    if hasattr(current_user, 'id'):
+        identity.provides.add(UserNeed(current_user.id))
+
+    if hasattr(current_user, "type"):  # it is a custom user class
+        if current_user.type == "account":  # it is an account, so it can have roles
+            account = db.session.query(Account).filter(Account.id == current_user.id).first()
+            for role in account.roles:
+                logger.info("Add role %s", role.name)
+                identity.provides.add(RoleNeed(role.name))
+
+
+@login_manager.unauthorized_handler
+def unauthorized_ogb():
+    """
+    Handle unauthorized users that visit with an out of game browser
+    -> Redirect them to SSO
+    """
+    return get_sso_redirect('linelogin', '')
+
+@app.template_filter('waittime')
+def jinja2_waittime_filter(value):
+    currentUTC = datetime.utcnow()
+    waitedTime = currentUTC-value
+    return str(int(math.floor(waitedTime.total_seconds()/60)))
---
+++ waitlist/utility/jinja2/__init__.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+from flask import request
+from flask.ext.login import current_user
+
+from waitlist.base import app
+from waitlist.data import version
+from waitlist.permissions import perm_manager
+from waitlist.utility.account import get_user_type
+from waitlist.utility.config import cdn_eveimg, cdn_eveimg_webp, cdn_eveimg_js
+from waitlist.utility.settings.settings import sget_insert
+from waitlist.data.perm import perm_management, perm_settings, perm_admin,\
+    perm_officer, perm_accounts, perm_feedback, perm_dev, perm_leadership,\
+    perm_bans, perm_viewfits, perm_comphistory, perm_mod_mail_resident,\
+    perm_mod_mail_tbadge
+
+
+def eve_image(browser_webp):
+    if browser_webp and cdn_eveimg_webp:
+        def _eve_image(path, suffix):
+            return cdn_eveimg.format(path, 'webp')
+    else:
+        def _eve_image(path, suffix):
+            return cdn_eveimg.format(path, suffix)
+    return _eve_image
+
+
+# set if it is the igb
+@app.context_processor
+def inject_data():
+    is_account = False
+    if hasattr(current_user, 'type'):
+        is_account=(current_user.type == "account")
+    header_insert = sget_insert('header')
+
+    currentTime = datetime.utcnow()
+    endTime = datetime(2016, 8, 7, 11, 0, 0)
+    startTime = datetime(2016, 7, 4, 11, 0, 0)
+    cc_vote_on = (currentTime < startTime and currentTime > endTime)
+
+    if (header_insert is not None):
+        header_insert = header_insert.replace("$type$", str(get_user_type()))
+    if 'image/webp' in request.headers.get('accept'):
+        reqSupportsWebp = True
+    else:
+        reqSupportsWebp = False
+    eve_image_macro = eve_image(reqSupportsWebp)
+    return dict(perm_admin=perm_admin, perm_settings=perm_settings,
+                perm_man=perm_management, perm_officer=perm_officer,
+                perm_accounts=perm_accounts, perm_feedback=perm_feedback,
+                is_account=is_account, perm_dev=perm_dev, perm_leadership=perm_leadership,
+                perm_bans=perm_bans, perm_viewfits=perm_viewfits, version=version,
+                perm_comphistory=perm_comphistory, perm_res_mod=perm_mod_mail_resident,
+                perm_t_mod=perm_mod_mail_tbadge, perm_manager=perm_manager, header_insert=header_insert,
+                eve_proxy_js=cdn_eveimg_js, eve_cdn_webp=cdn_eveimg_webp, browserSupportsWebp=reqSupportsWebp,
+                eve_image=eve_image_macro, ccvote_on=cc_vote_on
+                )
