88ad05554ba6c39a39df7697095659f3a24e4d9d
SpeedProg <speedprogde@googlemail.com>
2017-04-18 23:41:26 +0200

    improved esi failure handling

--- waitlist/utility/outgate/alliance/info.py
+++ waitlist/utility/outgate/alliance/info.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 
 from waitlist.storage.database import APICacheAllianceInfo
+from waitlist.utility.outgate.exceptions import check_esi_response
 from waitlist.utility.swagger.eve.alliance import AllianceEndpoint, AllianceInfo
 from waitlist import db
 
@@ -16,24 +17,21 @@ def set_from_alliance_info(self: APICacheAllianceInfo, info: AllianceInfo):
     self.expire = info.expires()
 
 
-def get_alliance_info(alliance_id: int) -> APICacheAllianceInfo:
+def get_alliance_info(alliance_id: int, *args) -> APICacheAllianceInfo:
     all_cache: APICacheAllianceInfo = db.session.query(APICacheAllianceInfo) \
         .filter(APICacheAllianceInfo.id == alliance_id).first()
 
     if all_cache is None:
         all_cache = APICacheAllianceInfo()
         all_ep = AllianceEndpoint()
-        all_info = all_ep.get_alliance_info(alliance_id)
-        if all_info is None:
-            # this should never happen
-            logger.error(f'No Alliance with id {alliance_id} exists!')
+        all_info: AllianceInfo = check_esi_response(all_ep.get_alliance_info(alliance_id), get_alliance_info, args)
 
         set_from_alliance_info(all_cache, all_info)
         db.session.add(all_cache)
         db.session.commit()
     elif all_cache.characterName is None:
         all_ep = AllianceEndpoint()
-        all_info = all_ep.get_alliance_info(alliance_id)
+        all_info: AllianceInfo = check_esi_response(all_ep.get_alliance_info(alliance_id), get_alliance_info, args)
         set_from_alliance_info(all_cache, all_info)
         db.session.commit()
     else:
@@ -41,7 +39,7 @@ def get_alliance_info(alliance_id: int) -> APICacheAllianceInfo:
         if all_cache.expire is None or all_cache.expire < now:
             # expired, update it
             all_ep = AllianceEndpoint()
-            all_info = all_ep.get_alliance_info(alliance_id)
+            all_info: AllianceInfo = check_esi_response(all_ep.get_alliance_info(alliance_id), get_alliance_info, args)
             set_from_alliance_info(all_cache, all_info)
             db.session.commit()
 
