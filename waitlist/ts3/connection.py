51533a189b136997817ea7ad2c842dab25aab68d
SpeedProg <speedprogde@googlemail.com>
2017-04-18 18:56:29 +0200

    fixed a poke failing

--- waitlist/ts3/connection.py
+++ waitlist/ts3/connection.py
@@ -93,7 +93,7 @@ def send_poke(name, msg):
     found = False
     for resp in response:
         if resp['client_nickname'] == name:
-            conn.clientpoke(msg, clid=resp['clid'], msg=msg)
+            conn.clientpoke(clid=resp['clid'], msg=msg)
             found = True
     # deaf people put a * in front
     if not found:
