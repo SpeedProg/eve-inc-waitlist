201f5b2c81e44d00dd1d57e4dc8f5cdcfad07312
SpeedProg <speedprogde@googlemail.com>
2017-05-22 23:00:21 +0200

    fixed some anotations to not be overspecific anymore

--- waitlist/utility/outgate/exceptions/__init__.py
+++ waitlist/utility/outgate/exceptions/__init__.py
@@ -1,4 +1,4 @@
-from typing import Callable, Any, Optional, Sequence
+from typing import Callable, Any, Optional, Sequence, List
 
 import logging
 
@@ -17,18 +17,18 @@ class ESIException(ApiException):
     Base class for sending esi based api exceptions
     """
 
-    def __init__(self, error_response: Optional[ESIResponse], call: Callable[Any, Any]) -> None:
+    def __init__(self, error_response: Optional[ESIResponse], call: Callable) -> None:
         self.__resp = error_response
         self.__call = call
 
     def get_response(self) -> Optional[ESIResponse]:
         return self.__resp
 
-    def get_call(self) -> Callable[Any, Any]:
+    def get_call(self) -> Callable:
         return self.__call
 
 
-def check_esi_response(resp: Optional[ESIResponse], call: Callable[Any, Any], params: Sequence[Any]) -> ESIResponse:
+def check_esi_response(resp: Optional[ESIResponse], call: Callable, params: Sequence[Any]) -> ESIResponse:
     if resp is None:
         # this should never happen
         logger.error(f'Got no response in {call.__name__} with {params}')
