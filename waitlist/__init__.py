715dfc091e8aa5d2a57c41a235642f86983cdce5
SpeedProg <speedprogde@googlemail.com>
2018-05-27 13:00:24 +0200

    changed swagger config to use ' instead of " and set uiversion to 3

--- waitlist/__init__.py
+++ waitlist/__init__.py
@@ -109,31 +109,34 @@ class MiniJSONEncoder(JSONEncoder):
 app.json_encoder = MiniJSONEncoder
 
 # init rate limiting
-limiter = Limiter(key_func=get_ipaddr, storage_uri="memory://", strategy="moving-window")
+limiter = Limiter(key_func=get_ipaddr, storage_uri="memory://",
+                  strategy="moving-window")
 limiter.init_app(app)
 
 app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "title": "Swagger Waitlist API",
-    "headers": [],
-    "specs": [
+    'swagger_version': '2.0',
+    'title': 'Swagger Waitlist API',
+    'headers': [],
+    'specs': [
         {
-            "version": "0.0.1",
-            "title": "Api v1",
-            "endpoint": 'spec/',
-            "description": 'Version 1 of the Swagger Waitlist API',
-            "route": '/spec/v1/swagger.json',
+            'version': '0.0.1',
+            'title': 'Api v1',
+            'endpoint': 'spec/',
+            'description': 'Version 1 of the Swagger Waitlist API',
+            'route': '/spec/v1/swagger.json',
             # rule_filter is optional
             # it is a callable to filter the views to extract
-            "rule_filter": lambda rule: (print(rule.endpoint) or '_v1' in rule.endpoint),
+            'rule_filter': lambda rule: (print(rule.endpoint) or
+                                         '_v1' in rule.endpoint),
             # definition_filter is optional
             # it is a callable to filter the definition models to include
-            "definition_filter": lambda definition: (
+            'definition_filter': lambda definition: (
                 'v1_model' in definition.tags)
         }
     ],
-    "host": config.domain,
-    "basePath": ""
+    'host': config.domain,
+    'basePath': '',
+    'uiversion': 3,
 }
 
 swag = Swagger(app)
