0ed6845c0d37f25f3966c7e8446ff5ac950708c9
SpeedProg <speedprogde@googlemail.com>
2018-06-21 10:14:36 +0200

    change eve-ui sent local with language

--- static/js/eve-ui-config.js
+++ static/js/eve-ui-config.js
@@ -6,10 +6,19 @@ var eveui_imageserver = function(image_ref) {
 	return eve_image(encodeURI(image_ref), 'png');
 };
 // fix for getting names for modules from ccp api that are in the users browser language
-$.ajaxSetup({
-	beforeSend: function(xhr, settings) {
-		if (settings.url.includes('/universe/types/')) {
-			xhr.setRequestHeader('Accept-Language', 'en-us, en;q=0.9');
-		}
+{
+	let lang_code = document.getElementById('lang-code').getAttribute('content');
+	
+	// esi only knows en_us (maybe it accepts en_* too but lets better be sure
+	if (lang_code.startsWith('en_') || lang_code == 'en') {
+		lang_code = 'en_us';
 	}
-});
+
+	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (settings.url.includes('/universe/types/')) {
+				xhr.setRequestHeader('Accept-Language', lang_code);
+			}
+		}
+	});
+}
