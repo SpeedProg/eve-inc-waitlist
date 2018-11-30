3b5462171c7950bbae539656b62f5a6256cf2379
Beryl <berylslanjava@icloud.com>
2018-11-30 13:51:41 +0000

    instantiate a waitlist swagger client within base module and make use of it

--- static/js/base.js
+++ static/js/base.js
@@ -4,68 +4,78 @@ if (!waitlist) {
 	var waitlist = {};
 }
 
-waitlist.base = (function(){
-	function getMetaData (name) {
-		return $('meta[name="'+name+'"]').attr('content');
-	}
+waitlist.base = (() => {
 
-	var wlClient = SwaggerClient(
-		{
+	function buildSwaggerClient(csrf) {
+		return SwaggerClient({
 			url: "/spec/v1/swagger.json",
-			requestInterceptor : function(req) {
-				req.headers['X-CSRFToken'] = getMetaData('csrf-token');
+			requestInterceptor : (req) => {
+				req.headers['X-CSRFToken'] = csrf;
 				return req;
 			}
-		}
-	);
-	
-	function displayMessage(message, type, html, id) {
-		var alertHTML = $($.parseHTML(`<div class="alert alert-dismissible alert-${type}" role="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-				</button>
-				<p class="text-center"></p>
-				</div>`));
-		var textContainer = $('.text-center', alertHTML);
-		if (typeof id !== "undefined") {
-			alertHTML.attr("id", id);
-		}
-		if (typeof html !== "undefined") {
-			textContainer.html(message);
-		} else {
-			textContainer.text(message);
-		}
-		var alertArea = $('#alert-area-base');
-		alertArea.append(alertHTML);
-	}
-	
-	function handleLanguageSelection(event) {
-		let langCode = event.target.value;
-		wlClient.then((client) => {
-			client.apis.I18n.put_i18n_locale({
-				'lang': langCode
-			}).then(() => {
-				location.reload();
-			}).catch(function(event) {
-				if (typeof(event.response) !== "undefined" &&
-					typeof(event.response.obj) !== "undefined" &&
-					typeof(event.response.obj.error) !== "undefined"){
-					waitlist.base.displayMessage($.i18n('wl-lang-change-failed', event.response.obj.error), "danger");
-				} else {
-					waitlist.base.displayMessage($.i18n('wl-add-alt-failed'), "danger");
-				}
-			});
 		});
 	}
-	
-	function init() {
-		$('#lang-select').on('change', handleLanguageSelection);
+
+	class WaitlistBase {
+		get client() {
+			if (!this.swagger_client) {
+				this.swagger_client = buildSwaggerClient(this.getMetaData('csrf-token')).catch((err) => {
+					this.swagger_client = null;
+					throw err;
+				});
+			}
+			return this.swagger_client;
+		}
+
+		getMetaData(name) {
+			return $(`meta[name="${name}"]`).attr('content');
+		}
+
+		displayMessage(message, type, html, id) {
+			var alertHTML = $($.parseHTML(`<div class="alert alert-dismissible alert-${type}" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+			<p class="text-center"></p>
+			</div>`));
+			var textContainer = $('.text-center', alertHTML);
+			if (typeof id !== "undefined") {
+				alertHTML.attr("id", id);
+			}
+			if (typeof html !== "undefined") {
+				textContainer.html(message);
+			} else {
+				textContainer.text(message);
+			}
+			var alertArea = $('#alert-area-base');
+			alertArea.append(alertHTML);
+		}
 	}
 	
-	$(document).ready(init);
+	const base = new WaitlistBase();
 
-	return {
-		getMetaData: getMetaData,
-		displayMessage: displayMessage
-	};
+	async function handleLanguageSelection(event) {
+		const langCode = event.target.value;
+		try {
+			const client = await base.client;
+			await client.apis.I18n.put_i18n_locale({
+				"lang": langCode
+			});
+			location.reload(true);
+		} catch (event) {
+			if (typeof(event.response) !== "undefined" &&
+				typeof(event.response.obj) !== "undefined" &&
+				typeof(event.response.obj.error) !== "undefined") {
+					base.displayMessage($.i18n('wl-lang-change-failed', event.response.obj.error), "danger");
+			} else {
+				base.displayMessage($.i18n('wl-lang-change-failed', langCode), "danger");
+			}
+		}
+	}
+
+	$(document).ready(() =>
+		document.getElementById("lang-select")
+			.addEventListener("change", handleLanguageSelection));
+
+	return base;
 })();
