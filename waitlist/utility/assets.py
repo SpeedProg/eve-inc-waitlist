83b9087b97d12b14e2af576e6d62acff18e80507
Beryl <berylslanjava@icloud.com>
2019-04-08 08:06:18 +0100

    move client i18n into single static file per locale

--- waitlist/utility/assets.py
+++ waitlist/utility/assets.py
@@ -1,6 +1,8 @@
+import json
 from os import path
 from webassets.bundle import Bundle
 from waitlist.utility.webassets.loader.jinja2 import Jinja2Loader
+from waitlist.utility.webassets.filter.jinja2 import CacheableJinja2Filter
 from flask_assets import Environment
 
 
@@ -15,19 +17,22 @@ def register_asset_bundles(assets: Environment):
                             filters='csscomp',
                             output='gen/themes/default.%(version)s.css')
 
-    i18n_de = Bundle('local/de.json', filters="jsonmin",
-                     output='gen/local/de.%(version)s.json')
-    i18n_en = Bundle('local/en.json', filters="jsonmin",
-                     output='gen/local/en.%(version)s.json')
-
     bundles = {
         'themes.dark': themes_dark,
         'themes.dark_purple': themes_darkpurple,
         'themes.default': themes_default,
-        'i18n.de': i18n_de,
-        'i18n.en': i18n_en,
     }
     assets.register(bundles)
+
+    translations = []
+    for locale in assets.app.babel_instance.list_translations():
+        LocaleJinja2Bundle(
+            assets,
+            path.join(assets.app.static_folder, 'local', locale.language + '.json'),
+        )
+        translations.append((locale.language, locale.get_language_name()))
+
+    assets.app.jinja_env.globals.update(translations=translations)
 
     # bundles in templates should be preparsed when not auto building bundles
     if not assets.auto_build:
@@ -46,3 +51,39 @@ def register_asset_bundles(assets: Environment):
 
         # register bundles with asset environment
         assets.add(*[bundle for bundle in bundles if not bundle.is_container])
+
+
+class LocaleJinja2Bundle():
+
+    def __init__(self, assets: Environment, locale_path):
+        self.path = locale_path
+        self.lang_code = path.basename(locale_path).replace('.json', '')
+        assets.register(
+            self.lang_code,
+            'js/i18n.js',
+            filters=(
+                CacheableJinja2Filter(self, assets.app.jinja_env),
+                'babili',
+            ),
+            output='gen/js/i18n.' + self.lang_code + '.%(version)s.js',
+        )
+
+    def unique(self):
+        return {
+            'lang_code': self.lang_code,
+            'lang_mtime': '{}'.format(path.getmtime(self.path)),
+        }
+
+    def context(self):
+        return {
+            'lang_code': self.lang_code,
+            'lang_json': json.dumps(self.validate(), separators=(',', ':'), ensure_ascii=False),
+        }
+
+    def validate(self):
+        with open(self.path, 'r', encoding='utf-8') as fp:
+            lang = json.load(fp)
+            if not ('@metadata' in lang and 'locale' in lang['@metadata']):
+                raise Exception('unable to validate {} as locale file for {}'.format(self.path, self.lang_code))
+            return lang
+
