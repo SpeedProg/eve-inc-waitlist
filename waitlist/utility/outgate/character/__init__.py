eae8f288b20adecd8344d03ac80431c8d60ca113
Beryl <berylslanjava@icloud.com>
2019-10-04 20:59:10 +0100

    replace char affiliation fn with char info fn

--- waitlist/utility/outgate/character/__init__.py
+++ waitlist/utility/outgate/character/__init__.py
@@ -1,4 +1,3 @@
 from .info import get_character_info as get_info
 from .info import get_character_info_by_name as get_info_by_name
 from .info import get_char_or_corp_or_alliance_id_by_name as get_char_corp_all_id_by_name
-from .info import get_char_affiliations as get_affiliations
